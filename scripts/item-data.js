407861ca8e76d539172f4518f3c6d21c4e315542
Mike Bostone <mikebostone@gmail.com>
2016-04-13 15:24:24 -0500

    building JS

---
+++ scripts/item-data.js
@@ -0,0 +1,21 @@
+mbPagination.settings.items = [{
+	id: "winter-jacket-1",
+	name: "1 Winter Jacket",
+	image: "images/placeholder-image.png",
+	price: 179.99,
+	salePrice: 3
+},
+{
+	id: "winter-jacket-2",
+	name: "2 Winter Jacket",
+	image: "images/placeholder-image.png",
+	price: 179.99,
+	salePrice: 1
+},
+{
+	id: "winter-jacket-3",
+	name: "3 Winter Jacket",
+	image: "images/placeholder-image.png",
+	price: 179.99,
+	salePrice: 2
+}]
--- scripts/mbPagination.js
+++ scripts/mbPagination.js
@@ -8,6 +8,63 @@ var mbPagination = {};
 (function($) {
 	mbPagination = {
 		settings: {
+			items: [],
+			sortedItems: [],
+			sortMethods: ['Featured', 'Name', 'Price Low to High', 'Price High to Low'],
+			sortMethod: 'Featured',
+			itemsPerPage: 24
+		},
+		orderItems: function() {
+
+			mbPagination.settings.sortedItems = [];
+
+			switch(mbPagination.settings.sortMethod) {
+				case 'Featured':
+					mbPagination.settings.sortedItems = mbPagination.settings.items;
+					console.log('featured');
+					break;
+				case 'Name':
+					console.log('name');
+					break;
+				case 'Price Low to High':
+					mbPagination.settings.sortedItems = mbPagination.settings.items.slice().sort(mbPagination.sortPriceLowToHigh);
+					console.log('price low to high');
+					break;
+				case 'Price High to Low':
+					console.log('price high to low');
+					break;
+			}
+
+		},
+		sortPriceLowToHigh: function(a,b) {
+			// should always sort with sale price. If no sale price is present store we will pass regular price in as sale price
+			return a.salePrice-b.salePrice;
+		},
+		sortPriceHighToLow: function(a, b) {
+			// should always sort with sale price. If no sale price is present store we will pass regular price in as sale price
+			return b.salePrice-a.salePrice;
+		},
+		buildItemHTML: function() {
+
+			$.ajax({
+				url: 'templates/item-box.handlebars',
+				dataType: 'html',
+				cache: false,
+				success: function(data, status, response) {
+
+					//console.log('template loaded');
+
+					var template = Handlebars.compile(response.responseText);
+					var itemHTML;
+					
+					mbPagination.settings.sortedItems.forEach(function(item){
+						itemHTML += template(item);
+					});
+
+					$('#quick-view-popup').html(itemHTML);
+
+				}
+			});
 
 		},
 		showItemImages: function() {
@@ -17,8 +74,17 @@ var mbPagination = {};
 			});
 
 		},
-		updateResults: function() {
+		updateResults: function(sortMethod, itemsPerPage) {
 
+			if(sortMethod) {
+				mbPagination.settings.sortMethod = sortMethod;
+			}
+			if(itemsPerPage) {
+				mbPagination.settings.itemsPerPage = itemsPerPage;
+			}
+
+			mbPagination.orderItems();
+			mbPagination.buildItemHTML();
 			mbPagination.showItemImages();
 
 		},
@@ -32,7 +98,3 @@ var mbPagination = {};
 		}
 	}
 })(jQuery)
-
-$(document).ready(function() {
-	mbPagination.go();
-});
